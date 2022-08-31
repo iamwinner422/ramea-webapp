a454a3b3e68d29b5aea9ccd2babd0c24b543ac54
iamwinner422 <iamwinner422@gmail.com>
2022-08-31 20:12:25 +0000

    Ajout du model dans l'application requestings

--- requestings/models.py
+++ requestings/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Requestings(models.Model):
+    nom = models.CharField(max_length=255)
+    telephone_p = models.CharField(max_length=8)
+    telephone_s = models.CharField(null=True, max_length=8, blank=True)
+    mail = models.CharField(max_length=255)
+    other = models.TextField(null=True, blank=True)
+    date_add = models.DateTimeField(auto_now_add=True)
