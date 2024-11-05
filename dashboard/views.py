4cc875468264d9b25e74aa9b3aa0425629950bd8
422.io <iamwinner422@gmail.com>
2024-11-05 16:49:17 +0000

    Update views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -32,34 +32,34 @@ def index(request):
     nb_prest = Prestations.objects.filter(org_id=admin_profile.organisation_id).count()
     nb_clt = Clients.objects.filter(org_id=admin_profile.organisation_id).count()
     #TOP DES CLIENTS
-    top_clt_vente = Clients.objects.filter(org=org, ventes__org=org).values('id','nom', 'prenoms', 'point_vente__nom').annotate(montant=Sum('ventes__montant_net')).order_by('-montant', 'nom')[:10] #PAR MONTANT DE L'ACHAT
+    #top_clt_vente = Clients.objects.filter(org=org, ventes__org=org).values('id','nom', 'prenoms', 'point_vente__nom').annotate(montant=Sum('ventes__montant_net')).order_by('-montant', 'nom')[:10] #PAR MONTANT DE L'ACHAT
 
     #TOP DES CLIENTS 
-    top_clt_prest = Clients.objects.filter(org=org, prestations__org=org).values('id','nom', 'prenoms', 'point_vente__nom').annotate(montant=Sum('prestations__total_prestation')).order_by('-montant', 'nom')[:10] #PAR MONTANT DE LA PRESTATION
+    #top_clt_prest = Clients.objects.filter(org=org, prestations__org=org).values('id','nom', 'prenoms', 'point_vente__nom').annotate(montant=Sum('prestations__total_prestation')).order_by('-montant', 'nom')[:10] #PAR MONTANT DE LA PRESTATION
  
     #PRODUIT LES PLUS VENDUS DE TOUTE L'ORGANISATION DU JOUR
-    most_sell_j = Produits.objects.filter(org=org, ventes__org=org, ventes__date_vente__year=today.year, ventes__date_vente__month=today.month, ventes__date_vente__day=today.day).values('id','designation', 'ventes__point_vente__nom').annotate(qte=Sum('produitvente__qte_cmdee')).order_by('-qte')[:10]
+    #most_sell_j = Produits.objects.filter(org=org, ventes__org=org, ventes__date_vente__year=today.year, ventes__date_vente__month=today.month, ventes__date_vente__day=today.day).values('id','designation', 'ventes__point_vente__nom').annotate(qte=Sum('produitvente__qte_cmdee')).order_by('-qte')[:10]
     
     #PRODUIT LES PLUS VENDUS DE TOUTE L'ORGANISATION DU MOIS
-    most_sell_m = Produits.objects.filter(org=org, ventes__org=org, ventes__date_vente__year=today.year, ventes__date_vente__month=today.month).values('id','designation', 'ventes__point_vente__nom').annotate(qte=Sum('produitvente__qte_cmdee')).order_by('-qte')[:10]
+    #most_sell_m = Produits.objects.filter(org=org, ventes__org=org, ventes__date_vente__year=today.year, ventes__date_vente__month=today.month).values('id','designation', 'ventes__point_vente__nom').annotate(qte=Sum('produitvente__qte_cmdee')).order_by('-qte')[:10]
 
     #TOP DES VENDEURS PAR POINT DE VENTE/MOIS SELON LE NOMBRE DE VENTE
-    top_vendeur_m_p = Utilisateurs.objects.filter(organisation=org, ventes__org=org, ventes__date_vente__year=today.year, ventes__date_vente__month=today.month).values('id', 'user__last_name', 'user__first_name', 'ventes__point_vente__nom').annotate(nb=Count('ventes__gerant')).order_by('-nb')[:10]
+    #top_vendeur_m_p = Utilisateurs.objects.filter(organisation=org, ventes__org=org, ventes__date_vente__year=today.year, ventes__date_vente__month=today.month).values('id', 'user__last_name', 'user__first_name', 'ventes__point_vente__nom').annotate(nb=Count('ventes__gerant')).order_by('-nb')[:10]
 
     #TOP DES VENDEURS PAR POINT DE VENTE/MOIS SELON LE NOMBRE DE PRESTATION
-    top_vendeur_m_s = Utilisateurs.objects.filter(organisation=org, prestations__org=org, prestations__date_prestation__year=today.year, prestations__date_prestation__month=today.month).values('id', 'user__last_name', 'user__first_name', 'prestations__point_vente__nom').annotate(nb=Count('prestations__gerant')).order_by('-nb')[:10]
+    #top_vendeur_m_s = Utilisateurs.objects.filter(organisation=org, prestations__org=org, prestations__date_prestation__year=today.year, prestations__date_prestation__month=today.month).values('id', 'user__last_name', 'user__first_name', 'prestations__point_vente__nom').annotate(nb=Count('prestations__gerant')).order_by('-nb')[:10]
 
     #TOP DES SERVICES DU MOIS
-    top_serv_m = Services.objects.filter(org=org, prestations__org=org, prestations__date_prestation__year=today.year, prestations__date_prestation__month=today.month).values('id','nom', 'prestations__point_vente__nom').annotate(nb=Count('prestations__service')).order_by('-nb')[:10]
+    #top_serv_m = Services.objects.filter(org=org, prestations__org=org, prestations__date_prestation__year=today.year, prestations__date_prestation__month=today.month).values('id','nom', 'prestations__point_vente__nom').annotate(nb=Count('prestations__service')).order_by('-nb')[:10]
 
     #TOP DES SERVICES DU JOUR
-    top_serv_j = Services.objects.filter(org=org, prestations__org=org, prestations__date_prestation__year=today.year, prestations__date_prestation__month=today.month, prestations__date_prestation__day=today.day).values('id','nom', 'prestations__point_vente__nom').annotate(nb=Count('prestations__service')).order_by('-nb')[:10]
+    #top_serv_j = Services.objects.filter(org=org, prestations__org=org, prestations__date_prestation__year=today.year, prestations__date_prestation__month=today.month, prestations__date_prestation__day=today.day).values('id','nom', 'prestations__point_vente__nom').annotate(nb=Count('prestations__service')).order_by('-nb')[:10]
 
     #CLASSEMENT DES POINTS DE VENTE PAR VENTE #DU JOUR
-    top_pdv_j = PointsAffaires.objects.filter(org=org, ventes__org=org, ventes__date_vente__year=today.year, ventes__date_vente__month=today.month, ventes__date_vente__day=today.day).values('id', 'nom').annotate(chiffre=Sum('ventes__montant_net')).order_by('-chiffre')[:10]
+    #top_pdv_j = PointsAffaires.objects.filter(org=org, ventes__org=org, ventes__date_vente__year=today.year, ventes__date_vente__month=today.month, ventes__date_vente__day=today.day).values('id', 'nom').annotate(chiffre=Sum('ventes__montant_net')).order_by('-chiffre')[:10]
 
     #CLASSEMENT DES POINTS DE VENTE PAR VENTE #DU MOIS    
-    top_pdv_m = PointsAffaires.objects.filter(org=org, ventes__org=org, ventes__date_vente__year=today.year, ventes__date_vente__month=today.month).values('id', 'nom').annotate(chiffre=Sum('ventes__montant_net')).order_by('-chiffre')[:10]
+    #top_pdv_m = PointsAffaires.objects.filter(org=org, ventes__org=org, ventes__date_vente__year=today.year, ventes__date_vente__month=today.month).values('id', 'nom').annotate(chiffre=Sum('ventes__montant_net')).order_by('-chiffre')[:10]
 
     #CLASSEMENT DES POINTS DE VENTE PAR VENTE #DE L'ANNEE
     #top_pdv_a = PointsAffaires.objects.filter(org=org, ventes__org=org, ventes__date_vente__year=today.year).values('id', 'nom').annotate(chiffre=Sum('ventes__montant_net')).order_by('-chiffre')[:5]
@@ -77,17 +77,6 @@ def index(request):
         'nb_serv':nb_serv,
         'nb_vte':nb_vte,
         'nb_prest':nb_prest,
-        'top_clt_vente':top_clt_vente,
-        'top_clt_prest':top_clt_prest,
-        'most_sell_j':most_sell_j,
-        'lst_prest':lst_prest,
-        'top_pdv_m':top_pdv_m,
-        'top_pdv_j':top_pdv_j,
-        'most_sell_m': most_sell_m,
-        'top_vendeur_m_s':top_vendeur_m_s,
-        'top_vendeur_m_p':top_vendeur_m_p,
-        'top_serv_m':top_serv_m,
-        'top_serv_j':top_serv_j,
-        'nb_clt':nb_clt,
+        
     }
     return render(request, 'dashboard/layouts/index.html', context)
